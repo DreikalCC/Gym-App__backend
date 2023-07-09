a05fffb44272f605866be1ac921c72c021a63def
Aldo Villegas <avgaccs@gmail.com>
2023-07-09 16:28:15 -0700

    Update usersController.js

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -78,7 +78,7 @@ module.exports.selectTrainer = (req, res, next) => {
   console.log("body in select", req.body);
   const userId = req.user._id;
   const { trainer } = req.body;
-  User.findByIdAndUpdate({ _id: userId }, { trainer: trainer }, { new: true })
+  User.findByIdAndUpdate({ _id: userId }, { trainer: trainer })
     .orFail(onOrFail)
     .then(() => User.findById(userId))
     .then((data) => {
