f2852208cfa695afc601d3b26a4322802907b767
Aldo Villegas <avgaccs@gmail.com>
2023-07-09 15:17:49 -0700

    Update usersController.js

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -78,11 +78,7 @@ module.exports.selectTrainer = (req, res, next) => {
   console.log("body in select", req.body);
   const userId = req.user._id;
   const { trainer } = req.body;
-  User.updateOne(
-    { _id: userId },
-    { $addToSet: { trainer: trainer } },
-    { new: true }
-  )
+  User.updateOne({ _id: userId }, { trainer: trainer })
     .orFail(onOrFail)
     .then(() => User.findById(userId))
     .then((data) => {
